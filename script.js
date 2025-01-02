ae2a5f9b4bab05b3bda0bead085fcfebe3e2ec9b
Talent-1 <itzmhiztatalent@gmail.com>
2025-01-02 16:58:13 +0100

    updated

--- script.js
+++ script.js
@@ -16,14 +16,14 @@ const darkModeToggle = document.querySelector('.dark-mode-toggle');
 // Add event listener to the toggle button
 darkModeToggle.addEventListener('click', () => {
     //Toggle dark mode class on the root element
-    root.classList.toggle('.dark-mode-');
+    root.classList.toggle('dark-mode-toggle');
 });
 
 //Scroll to the top button
-const scrollTopButton = document.getElementsByClassName('scroll-to-top');
+const scrollTopButton = document.getElementsByClassName('scroll-to-top')[0];
 window.addEventListener('scroll', () => {
     if (window.scrollY > 500) {
-        scrollTopButton.stlye.display = 'block';
+        scrollTopButton.style.display = 'block';
     } else {
         scrollTopButton.style.display = 'none';
     }
